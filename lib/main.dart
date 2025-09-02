ebe8cd789513b8a6dba2b697b9a21675d9c9b84c
ajomobiswipe <130539051+ajomobiswipe@users.noreply.github.com>
2025-09-02 11:39:31 +0530

    main cleared

--- lib/main.dart
+++ lib/main.dart
@@ -33,8 +33,6 @@ import 'package:provider/provider.dart';
 CustomAlert customAlert = CustomAlert();
 AlertService alertService = AlertService();
 
-/// change from office pc
-/// change from office pc
 /// The main entry point of the application.
 void main() {
   runZonedGuarded<Future<void>>(() async {
